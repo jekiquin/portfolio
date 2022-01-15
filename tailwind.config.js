46db8d234c63a4a6ab6c8481ffe53eca49295467
Jerick Iquin - Home <jci.dev.0@gmail.com>
2022-01-14 20:09:46 -0800

    added base plugins on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
+const globalPlugins = ({ addBase, config }) => {
+	addBase({
+		body: {
+			fontFamily: config('theme.fontFamily.montserrat')
+		},
+		h1: {
+			fontFamily: config('theme.fontFamily.norwester'),
+			marginBottom: config('theme.spacing.10'),
+			lineHeight: config('theme.lineHeight.normal'),
+			fontSize: config('theme.fontSize.8xl')
+		},
+		h2: {
+			fontFamily: config('theme.fontFamily.kollektif'),
+			marginBottom: config('theme.spacing.8'),
+			lineHeight: config('theme.lineHeight.normal'),
+			fontSize: config('theme.fontSize.5xl')
+		}
+	});
+};
+
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				norwester: 'Norwester, sans-serif',
+				kollektif: 'Kollektif, sans-serif',
+				montserrat: 'Montserrat, sans-serif'
+			}
+		}
 	},
-	plugins: []
+	plugins: [globalPlugins]
 };
