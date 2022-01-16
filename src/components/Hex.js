fee0adbe0e3a1b78bc22018464685801db5c6c0f
Jerick Iquin - Home <jci.dev.0@gmail.com>
2022-01-16 12:01:07 -0800

    refactored hex component

--- src/components/Hex.js
+++ src/components/Hex.js
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 export default function Hex({ image, label }) {
 	return (
 		<li className="Hex">
-			<a className="Hex__dummy-link" href="/" onClick={(e) => e.preventDefault()}>
-				<img className="Hex__img" src={image} alt={label} title={label} />
-			</a>
+			<img className="Hex__img" src={image} alt={label} title={label} />
 		</li>
 	);
 }
