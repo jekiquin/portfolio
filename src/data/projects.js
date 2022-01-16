91fd7ebac7e34000970c472069c26bf480eb63c3
Jerick Iquin - Home <jci.dev.0@gmail.com>
2022-01-16 15:16:44 -0800

    added project list

---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+export const PROJECTS = [
+	{
+		name: 'Jerick Iquin Portfolio',
+		role: 'Solo',
+		date: 'January 2022',
+		image: null,
+		description: 'Portfolio Project',
+		stacks: ['React', 'SASS', 'TailwindCSS'],
+		deployedURL: '',
+		githubURL: 'https://github.com/jekiquin/portfolio'
+	},
+	{
+		name: 'Virtual Arcade - Retro Games',
+		role: 'Solo',
+		date: 'October 2021',
+		image: null,
+		description:
+			'Capstone Project. Library of retro games - Space Invaders, Pong, Breakout, and indie game - Jojo.',
+		stacks: ['React', 'React Router', 'Axios', 'SASS', 'PhaserJs', 'NodeJs', 'Express'],
+		deployedURL: 'https://virtualarcade-client.herokuapp.com/home',
+		githubURL: 'https://github.com/jekiquin/jerick-iquin-capstone'
+	},
+	{
+		name: "Jojo's Adventure",
+		role: 'Pair Programmer',
+		date: 'September 2021',
+		image: null,
+		description: 'Text based web game.',
+		stacks: ['React', 'React Router', 'Axios', 'SASS', 'NodeJs', 'Express'],
+		deployedURL: '',
+		githubURL: 'https://github.com/jekiquin/team-ceci-jek.git'
+	},
+	{
+		name: 'BrainFlix',
+		location: 'BrainStation',
+		role: 'Solo',
+		date: 'September 2021',
+		image: null,
+		description: 'Bootcamp project. Video website prototype.',
+		stacks: ['React', 'React Router', 'Axios', 'SASS', 'NodeJs', 'Express'],
+		deployedURL: '',
+		githubURL: 'https://github.com/jekiquin/jerick-iquin-brainflix'
+	}
+];
