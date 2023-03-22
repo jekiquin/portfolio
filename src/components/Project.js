d270b3a5663e212fbfddaabbb2a0928ca7e8cfec
Jerick Iquin <jci.dev.0@gmail.com>
2023-03-22 11:45:33 -0700

    fixed issues with icons showing on project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,67 +4,71 @@ import { useState, useEffect, useRef } from 'react';
 import { projectObserver } from 'utils/utils';
 
 export default function Project({ project }) {
-	const [modalView, setModalView] = useState('modal');
-	const projectRef = useRef();
-	const { name, image, stacks, description, deployedURL, githubURL } = project;
-	const displayImage = image || PLACEHOLDER.src;
+  const [modalView, setModalView] = useState('modal--hide');
+  const projectRef = useRef();
+  const { name, image, stacks, description, deployedURL, githubURL } = project;
+  const displayImage = image || PLACEHOLDER.src;
 
-	useEffect(() => {
-		projectObserver.observe(projectRef.current);
-	}, []);
+  useEffect(() => {
+    projectObserver.observe(projectRef.current);
+  }, []);
 
-	const handleMouseEnter = () => {
-		setModalView('modal--view');
-	};
+  const handleMouseEnter = () => {
+    setModalView('modal--view');
+  };
 
-	const handleMouseLeave = () => {
-		setModalView('modal--hide');
-	};
+  const handleMouseLeave = () => {
+    setModalView('modal--hide');
+  };
 
-	return (
-		<div
-			ref={projectRef}
-			className="scale-0 w-80 h-80 rounded-xl overflow-hidden bg-light-blue shadow-2xl sm:w-96 sm:h-96"
-			onMouseEnter={handleMouseEnter}
-			onMouseLeave={handleMouseLeave}>
-			<div className="h-1/2 relative">
-				<img className="h-full" src={displayImage} alt={name} />
-				<div
-					className={`${modalView} scale-x-0 absolute top-0 left-0 h-full w-full p-4 flex flex-col justify-between items-center bg-trans-green text-white`}>
-					<h3>{description}</h3>
-					<div className="flex gap-4 justify-center items-center  text-4xl">
-						{deployedURL && (
-							<a
-								className="hover:text-blue"
-								href={deployedURL}
-								rel="noreferrer"
-								target="_blank">
-								<i className="fas fa-link"></i>
-							</a>
-						)}
-						{githubURL && (
-							<a
-								className="hover:text-blue"
-								href={githubURL}
-								rel="noreferrer"
-								target="_blank">
-								<i className="fab fa-github-square"></i>
-							</a>
-						)}
-					</div>
-				</div>
-			</div>
+  return (
+    <div
+      ref={projectRef}
+      className="scale-0 w-80 h-80 rounded-xl overflow-hidden bg-light-blue shadow-2xl sm:w-96 sm:h-96"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="h-1/2 relative">
+        <img className="h-full" src={displayImage} alt={name} />
+        <div
+          className={`${modalView} scale-x-0 absolute top-0 left-0 h-full w-full p-4 flex flex-col justify-between items-center bg-trans-green text-white`}
+        >
+          <h3>{description}</h3>
+          <div className="flex gap-4 justify-center items-center  text-4xl">
+            {deployedURL && (
+              <a
+                className="hover:text-blue"
+                href={deployedURL}
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i className="fas fa-link"></i>
+              </a>
+            )}
+            {githubURL && (
+              <a
+                className="hover:text-blue"
+                href={githubURL}
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-github-square"></i>
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
 
-			<div className="p-4 overflow-hidden text-ellipsis overflow-hidden">
-				<button onClick={handleMouseEnter}>
-					<h3 className="text-2xl underline text-red-400">{name}</h3>
-				</button>
-				<p>{stacks.join(', ')}</p>
-			</div>
-		</div>
-	);
+      <div className="p-4 overflow-hidden text-ellipsis overflow-hidden">
+        <button onClick={handleMouseEnter}>
+          <h3 className="text-2xl underline text-red-400">{name}</h3>
+        </button>
+        <p>{stacks.join(', ')}</p>
+      </div>
+    </div>
+  );
 }
 
 Project.propTypes = {
-	project: PropTypes.object.isRequired
+  project: PropTypes.object.isRequired,
 };
